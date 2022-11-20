6e7efceb893239c62e07b77930cfe740b0e77f55
Macarena Gabler <maca.skys@outlook.com>
2022-11-20 12:52:16 -0300

    Avance 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,19 @@ function myFunction () {
         navbar.classList.remove("sticky");
     }
     }
+
+function {
+        $('[data-toggle="tooltip"]').tooltip();
+    
+        $('#quienesomos').click(function(){
+            alert("Somos la mejor empresa de viajes")
+        });
+    
+        $('#destacados').click(function(){
+            alert("Comparte en los mejores lugares de Chile")
+        });
+        
+        $('#contactos').click(function(){
+            alert("Que esperas para ser parte de nosotros?")
+        });
+    }
